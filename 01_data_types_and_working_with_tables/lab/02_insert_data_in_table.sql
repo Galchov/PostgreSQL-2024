faebffcf42ece2192b65690da0b9e3a5779df1c7
Zdravko Galchov <zdravko.galchov@gmail.com>
2024-05-19 14:06:21 +0100

    Added the lab of 'Data Types and Working With Tables'

---
+++ 01_data_types_and_working_with_tables/lab/02_insert_data_in_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (first_name, last_name, salary, devices_number)
+VALUES
+    ('John', 'Smith', 42000, 10),
+    ('Andrew', 'Gallagher', 45000, 12),
+    ('Jeff', 'Green', 55000, 07);
